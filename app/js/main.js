d25b6a58124bfb81677ee07d5f50c8f128e5de16
soret <karimovminds@gmail.com>
2023-03-29 17:02:13 +0400

    added swiper slider, header, footer, basic styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$(function () {
-	
+// swiper slider
+let slider = document.querySelector(".slider");
+
+const swiper = new Swiper(slider, {
+	direction: "vertical",
+	slidesPerView: 1,
+	hashNavigation: true,
+
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
+
+	keyboard: {
+		enabled: true,
+		pageUpDown: true,
+	},
+	mousewheel: true,
 });
