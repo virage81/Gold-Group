1492e3d3c1a9bb9315aeb81f4a69df007b19f43e
soret <karimovminds@gmail.com>
2023-04-03 16:20:25 +0400

    added map

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,13 @@ const swiper = new Swiper(slider, {
 	mousewheel: true,
 });
 
+addEventListener("mouseover", (e) => {
+	if (e.target.className == "ymaps-2-1-79-events-pane ymaps-2-1-79-user-selection-none") {
+		swiper.mousewheel = false;
+		console.log(true);
+	}
+});
+
 topButton.addEventListener("click", () => {
 	popup.classList.add("show");
 });
@@ -27,3 +34,22 @@ topButton.addEventListener("click", () => {
 window.addEventListener("click", function (e) {
 	if (e.target == popup) popup.classList.remove("show");
 });
+
+// yandex map
+ymaps.ready(init);
+function init() {
+	// Создание карты.
+	let myPlacemark = new ymaps.Placemark([56.861243, 53.209738]);
+	let map = new ymaps.Map("map", {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [56.861243, 53.209738],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 17,
+		scroll: false,
+	});
+	map.geoObjects.add(myPlacemark);
+}
