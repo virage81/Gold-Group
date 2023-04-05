610572d5ad44b22a88f1510eeba85cd53a03c371
soret <karimovminds@gmail.com>
2023-04-05 04:41:48 +0400

    fixed number filed in form, adaptive on small height

--- app/js/main.js
+++ app/js/main.js
@@ -25,6 +25,35 @@ let swiper = new Swiper(slider, {
 	mousewheel: true,
 });
 
+// Маска номера телефона
+var eventCalllback = function (e) {
+	console.log(e);
+	var el = e.target,
+		clearVal = el.dataset.phoneClear,
+		pattern = el.dataset.phonePattern,
+		matrix_def = "+7(___) ___-__-__",
+		matrix = pattern ? pattern : matrix_def,
+		i = 0,
+		def = matrix.replace(/\D/g, ""),
+		val = e.target.value.replace(/\D/g, "");
+	if (clearVal !== "false" && e.type === "blur") {
+		if (val.length < matrix.match(/([\_\d])/g).length) {
+			e.target.value = "";
+			return;
+		}
+	}
+	if (def.length >= val.length) val = def;
+	e.target.value = matrix.replace(/./g, function (a) {
+		return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+	});
+};
+var phone_inputs = document.querySelectorAll("#phone");
+for (let elem of phone_inputs) {
+	for (let ev of ["input", "blur", "focus"]) {
+		elem.addEventListener(ev, eventCalllback);
+	}
+}
+
 // если курсор на карте, слайдер отключается
 addEventListener("mouseover", (e) => turnMapOff(e));
 addEventListener("touchstart", (e) => turnMapOff(e));
